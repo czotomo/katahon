b192e4f23dd2968e17ecbe458b4dcee4b4ee3ab3
Jakub <kuba.szecowka@gmail.com>
2017-12-02 12:06:18 +0100

    its fine

---
+++ gierka/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats
+{
+    [Range(0, 20)] public double PlJoy = 20;
+    [Range(0, 20)] public double PlHunger = 20;
+    [Range(0, 20)] public double PlTired = 20;
+    [Range(0, 20)] public double PlKnow = 20;
+    [Range(0, 20)] public double PlLuck = 20;
+    private double minSpeed = 1;
+
+    public double GetSpeed()
+    {
+        return minSpeed * (1 + PlHunger / 100) * (1 + PlTired / 100);
+    }
+}
