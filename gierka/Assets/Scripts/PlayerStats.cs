6162167f892e81833028b28231034f8fb2351299
Artur Szydłowski <artoro.pl@gmail.com>
2017-12-02 12:37:07 +0100

    changes in player stats

--- gierka/Assets/Scripts/PlayerStats.cs
+++ gierka/Assets/Scripts/PlayerStats.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
-public class PlayerStats
+public class PlayerStats : MonoBehaviour
 {
-    [Range(0, 20)] public float PlJoy = 20;
-    [Range(0, 20)] public float PlHunger = 20;
-    [Range(0, 20)] public float PlTired = 20;
-    [Range(0, 20)] public float PlKnow = 20;
-    [Range(0, 20)] public float PlLuck = 20;
-    private float minSpeed = 10;
+   [Range(0, 20)] public float PlJoy = 20;
+   [Range(0, 20)] public float PlHunger = 20;
+   [Range(0, 20)] public float PlTired = 20;
+   [Range(0, 20)] public float PlKnow = 20;
+   [Range(0, 20)] public float PlLuck = 20;
+   private const float MinSpeed = 1;
+   private const float DtHunger = 0.01f;
+   private const float DtTired = 0.01f;
 
-    public float GetSpeed()
-    {
-        return minSpeed * (1 + PlHunger / 100) * (1 + PlTired / 100);
-    }
+   void UpdateStats()
+   {
+       PlHunger -= DtHunger * Time.deltaTime;
+       if (PlHunger < 0) PlHunger = 0;
+       PlTired -= DtTired * Time.deltaTime;
+       if (PlTired < 0) PlTired = 0;
+   }
+
+   public float GetSpeed()
+   {
+       return MinSpeed * (1 + PlHunger / 100) * (1 + PlTired / 100);
+   }
+
+   public float GetGrade(string subject)
+   {
+       float randLuck = Random.Range(PlLuck/4, PlLuck);
+       return 2 * (1 + PlHunger/100) * (1 + PlTired/100) * (1 + PlJoy/100) * (1 + PlKnow/100) * (1 + randLuck / 100);
+   }
 }
