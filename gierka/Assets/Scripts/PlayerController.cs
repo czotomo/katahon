dd35e6330ec7fb1c4ba903852656cdddf0d64d63
Tomasz Bełtowski <beltowski.t@gmail.com>
2017-12-02 18:57:16 +0100

    wip

--- gierka/Assets/Scripts/PlayerController.cs
+++ gierka/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
-﻿using Assets.Scripts.Quests;
+﻿using System;
+using Assets.Scripts.Quests;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -8,17 +10,34 @@ namespace Assets.Scripts
         public PlayerStats PlayerStats;
         private BagelSpawner _bagelSpawner;
         private QuestManager _questManager;
+        private float _gameTime;
+        public Text gameOverText;
 
         void Start()
         {
+            gameOverText.gameObject.SetActive(false);
+            _gameTime = 0f;
             _questManager = FindObjectOfType<QuestManager>();
             _bagelSpawner = FindObjectOfType<BagelSpawner>();
         }
 
         void Update()
         {
+            _gameTime += Time.timeSinceLevelLoad;
             var moveVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             transform.position += new Vector3(moveVector.x, moveVector.y) * GetSpeed() * Time.deltaTime;
+            PlayerStats.UpdateStats();
+
+            if(PlayerStats.PlHunger <= 0.1f)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            gameOverText.gameObject.SetActive(true);
+            Time.timeScale = 0;
         }
 
         public float GetSpeed()
